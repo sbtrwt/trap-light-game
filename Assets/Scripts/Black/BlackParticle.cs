d5069fd798d899be9fd9cea41f7a5740552f4766
Shobhit Rawat <sbtrwt.se@gmail.com>
2023-11-16 16:59:21 +0530

    Added explosive feature

--- Assets/Scripts/Black/BlackParticle.cs
+++ Assets/Scripts/Black/BlackParticle.cs
@@ -8,9 +8,9 @@ public class BlackParticle : MonoBehaviour
     private Rigidbody2D rb2D;
 
     private LineRenderer line; // Reference to LineRenderer
-    private Vector3 mousePos;
     private Vector3 startPos;    // Start position of line
     private Vector3 endPos;    // End position of line
+    [SerializeField] private GameObject explosiveItemPrefab;
 
     private void Start()
     {
@@ -21,7 +21,7 @@ public class BlackParticle : MonoBehaviour
     {
         rb2D.MovePosition(rb2D.position + new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * Time.deltaTime * speed);
        
-        LineInput();
+        HandleInput();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -29,51 +29,24 @@ public class BlackParticle : MonoBehaviour
         //Debug.Log(collision.gameObject.tag);
     }
 
-    private void ValidatePosition()
-    {
-        if (GlobalConstant.MAX_HEIGHT < rb2D.position.y)
-        {
-            rb2D.position = new Vector2(rb2D.position.x, GlobalConstant.MIN_HEIGHT);
-        }
-        if (GlobalConstant.MIN_HEIGHT > rb2D.position.y)
-        {
-            rb2D.position = new Vector2(rb2D.position.x, GlobalConstant.MAX_HEIGHT);
-        }
-        if (GlobalConstant.MAX_WIDTH < rb2D.position.x)
-        {
-            rb2D.position = new Vector2(GlobalConstant.MIN_WIDTH, rb2D.position.y);
-        }
-        if (GlobalConstant.MIN_WIDTH > rb2D.position.x)
-        {
-            rb2D.position = new Vector2(GlobalConstant.MAX_WIDTH, rb2D.position.y);
-        }
-    }
-
-    void LineInput()
+    void HandleInput()
     {
 
-        // On mouse down new line will be created 
-        //if (Input.GetMouseButtonDown(0))
+      
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (line == null)
                CreateLine();
-            //mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            //mousePos.z = 0;
-            //line.SetPosition(0, mousePos);
-            //startPos = mousePos;
+          
             line.SetPosition(0, rb2D.position);
             startPos = rb2D.position;
         }
-        //else if (Input.GetMouseButtonUp(0))
+      
         else if (Input.GetKeyUp(KeyCode.Space))
         {
             if (line)
             {
-                //mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                //mousePos.z = 0;
-                //line.SetPosition(1, mousePos);
-                //endPos = mousePos;
+                
                 line.SetPosition(1, rb2D.position);
                 endPos = rb2D.position;
                 AddColliderToLine();
@@ -84,23 +57,24 @@ public class BlackParticle : MonoBehaviour
         {
             if (line)
             {
-                //mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                //mousePos.z = 0;
-                //line.SetPosition(1, mousePos);
+               
                 line.SetPosition(1, rb2D.position);
             }
         }
+
+        if(Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl))
+        {
+            CreateExplosive();
+        }
     }
-    // Following method creates line runtime using Line Renderer component
+
     private void CreateLine()
     {
         line = new GameObject("Line").AddComponent<LineRenderer>();
         line.material = new Material(Shader.Find("Diffuse"));
-        //line.SetVertexCount(2);
-        //line.SetWidth(0.1f, 0.1f);
-        //line.SetColors(Color.black, Color.black);
         line.useWorldSpace = true;
     }
+
     // Following method adds collider to created line
     private void AddColliderToLine()
     {
@@ -119,5 +93,11 @@ public class BlackParticle : MonoBehaviour
         }
         angle = Mathf.Rad2Deg * Mathf.Atan(angle);
         col.transform.Rotate(0, 0, angle);
+    }
+
+    private void CreateExplosive() 
+    {
+        if (explosiveItemPrefab != null)
+        Instantiate(explosiveItemPrefab, transform.position, Quaternion.identity );
     }
 }
--- Assets/Scripts/Explosion/ExplosiveItem.cs
+++ Assets/Scripts/Explosion/ExplosiveItem.cs
@@ -4,30 +4,63 @@ using UnityEngine;
 
 public class ExplosiveItem : MonoBehaviour
 {
-    [SerializeField]private float radius=10f;
+    [SerializeField] private float radius = 5f;
     [SerializeField] private ParticleSystem explosionEffectPrefab;
     private ParticleSystem explosionEffect;
+    private LineRenderer circleRange;
+    private int circleSteps = 360;
     private void Start()
     {
-        StartCoroutine("Detonate");
+        circleRange = GetComponent<LineRenderer>();
+
+        DrawCircle(circleSteps, radius);
+        StartCoroutine("StartAfterDelay");
     }
-    private IEnumerator Detonate() {
+    private void Detonate()
+    {
 
         Debug.Log("Inside detonate");
         explosionEffect = Instantiate(explosionEffectPrefab);
         explosionEffect.transform.position = gameObject.transform.position;
-       
+
         Vector2 position = transform.position;
         Collider2D[] colliders = Physics2D.OverlapCircleAll(position, radius);
-        foreach (Collider2D hit in colliders) {
-            Debug.Log("Inside detonate : "+hit.gameObject.tag);
-            if(hit.gameObject.CompareTag(GlobalConstant.LIGHT_TAG))
-                Destroy(hit.gameObject, 5f);
+        foreach (Collider2D hit in colliders)
+        {
+            Debug.Log("Inside detonate : " + hit.gameObject.tag);
+            if (hit.gameObject.CompareTag(GlobalConstant.LIGHT_TAG))
+                Destroy(hit.gameObject);
         }
-        Destroy(gameObject, 5f);
-
-        yield return new WaitForSeconds(5);
-
+        Destroy(gameObject);
         explosionEffect.Play();
     }
+
+    private IEnumerator StartAfterDelay()
+    {
+        yield return new WaitForSeconds(4);
+        Detonate();
+    }
+
+    private void DrawCircle(int steps, float radius)
+    {
+        if (circleRange == null) return;
+
+        circleRange.positionCount = steps;
+        float delta, rad, xScaled, yScaled, x, y;
+        Vector3 pos;
+        for (int i = 0; i < steps; i++)
+        {
+            delta = (float)i / steps;
+            rad = delta * 2 * Mathf.PI;
+            xScaled = Mathf.Cos(rad);
+            yScaled = Mathf.Sin(rad);
+            x = xScaled * radius + transform.position.x;
+            y = yScaled * radius + transform.position.y;
+
+            pos = new Vector3(x, y, 0);
+            circleRange.SetPosition(i, pos);
+        }
+
+    }
+
 }
